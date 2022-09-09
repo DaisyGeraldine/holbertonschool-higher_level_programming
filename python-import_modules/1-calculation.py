4a64dd91331950d41feddac958e75a0125540995
DaisyGeraldine <chipanal@hotmail.com>
2022-09-09 12:54:17 -0500

    add __main_ and fix pycodestyle code

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-print(a, "+",b ,"= {}".format(add(a, b)))
-print(a, "-",b ,"= {}".format(sub(a, b)))
-print(a, "*",b ,"= {}".format(mul(a, b)))
-print(a, "/",b ,"= {}".format(div(a, b)))
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print(a, "+", b, "= {}".format(add(a, b)))
+    print(a, "-", b, "= {}".format(sub(a, b)))
+    print(a, "*", b, "= {}".format(mul(a, b)))
+    print(a, "/", b, "= {}".format(div(a, b)))
