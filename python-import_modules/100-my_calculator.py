ff435dcfd5488914b239655acc425f20d2d63e22
DaisyGeraldine <chipanal@hotmail.com>
2022-09-11 22:51:34 -0500

    fix code of 1th conditional of

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,13 +2,13 @@
 if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
-    if len(argv) == 1:
+    if len(argv) != 4:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
     operator = {
         "+": add(int(argv[1]), int(argv[3])),
         "-": sub(int(argv[1]), int(argv[3])),
-        "*": mul(int(argv[1]), int(argv[3])),
+        '*': mul(int(argv[1]), int(argv[3])),
         "/": div(int(argv[1]), int(argv[3]))
         }
     error = "Unknown operator. Available operators: +, -, * and /"
