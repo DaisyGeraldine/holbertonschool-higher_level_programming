691a772a7acd2fcfb721c74131f6df8a1c5dc06b
DaisyGeraldine <chipanal@hotmail.com>
2022-09-17 19:32:23 -0500

    fix: function that prints x elements of a list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,9 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
         print()
-    except IndexError:
+        return i + 1
+    except (IndexError):
         print()
         return i
-    return i + 1
+    except (UnboundLocalError):
+        return 0
