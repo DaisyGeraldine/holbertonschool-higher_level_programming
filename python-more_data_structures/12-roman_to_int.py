0edf04a812d098bde07d1efd275a992ad80320de
DaisyGeraldine <chipanal@hotmail.com>
2022-09-18 22:30:11 -0500

    fix function that converts a Roman numeral to an integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if isinstance(roman_string, str) != True or roman_string is None:
+    if isinstance(roman_string, str) is not True or roman_string is None:
         return 0
     suma = 0
     # string counter according to condition of the next character
@@ -10,7 +10,6 @@ def roman_to_int(roman_string):
     string = ''.join(reversed(roman_string))
     table = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     while i < len(string):
-        #count += 1
         if count < len(string):
             if table[string[i]] > table[string[i + 1]]:
                 suma += (-1 * table[string[i + 1]]) + table[string[i]]
