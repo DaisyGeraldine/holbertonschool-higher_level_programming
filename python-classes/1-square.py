04dc82fc64a6fd03b2eb22d91fab704226aa79af
DaisyGeraldine <chipanal@hotmail.com>
2022-09-21 22:22:31 -0500

    modify comment of the file

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""
-This class named Square
+# -*- coding: utf-8 -*-
+"""This module named 1-Square.py
+   Created on Wednesday, September 21, 2022
+   @author: Daisy Chipana Lapa
+
 """
 
 
 class Square:
-    """
+    """This class is empty
 
-    This class is empty
-
+    Attributes:
+       Instantiation with size (no type/value verification)
     """
     def __init__(self, size=0):
-        """
+        """function constructor __init_
 
-        function constructor __init_
-        * size is private attribute
+        Args:
+             size: is private attribute
+        Returns:
+             Nothing
 
         """
         self.__size = size
