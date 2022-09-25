6715e81ee0451f698d92d8a8011aa23de1c0857c
DaisyGeraldine <chipanal@hotmail.com>
2022-09-25 17:41:45 -0500

    fix function that prints a square with the character #

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -16,8 +16,9 @@ def print_square(size):
     if isinstance(size, int):
         if size < 0:
             raise ValueError("size must be >= 0")
-    else:
-        if isinstance(size, float) and size < 0:
+    elif isinstance(size, float) and size < 0:
             raise TypeError("size must be an integer")
+    else:
+        raise TypeError("size must be an integer")
     for i in range(size):
         print("#" * size)
