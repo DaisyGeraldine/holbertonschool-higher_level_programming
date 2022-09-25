e7349958951779a7107acd144ba77fa030f66ace
DaisyGeraldine <chipanal@hotmail.com>
2022-09-24 20:58:58 -0500

    fix function that adds 2 integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This module 0-add_integer.py
+
+   Created on Saturday, September 24, 2022
+
+   @author: Daisy Chipana Lapa
+"""
+
+
 def add_integer(a, b=98):
+    """function add two numbers
+        Args:
+             a: is first parameter
+             b: is second parameter
+        Returns:
+             Sum two numbers cast as integers
+        """
     if isinstance(a, (int, float)) is False:
         raise TypeError("a must be an integer")
     if isinstance(b, (int, float)) is False:
