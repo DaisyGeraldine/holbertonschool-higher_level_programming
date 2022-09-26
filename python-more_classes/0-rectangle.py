31c93edc66cdfa3efaa3f5e11445c2d2a3bd09f9
DaisyGeraldine <chipanal@hotmail.com>
2022-09-26 08:56:19 -0500

    Empty class Rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This module named 0-rectangle.py
+   Created on Monday, September 26, 2022
+   @author: Daisy Chipana Lapa
+"""
+
+
+class Rectangle:
+    """This class is named rectangle
+    Attributes:
+       Empty class Rectangle
+    """
+    pass
