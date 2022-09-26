e89b6047dfcef898597cbb1a2d62f9ee66d09557
DaisyGeraldine <chipanal@hotmail.com>
2022-09-25 23:24:45 -0500

    function that prints a text with 2 new lines

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -20,7 +20,8 @@ def text_indentation(text):
     while i < len(text):
         print('{}'.format(text[i]), end="")
         if text[i] == '.' or text[i] == '?' or text[i] == ':':
-            if text[i] == ' ':
-                i += 1
-            print("\n")
+            if i != len(text) - 1:
+                if text[i + 1] == ' ':
+                    i += 1
+                print("\n")
         i += 1
