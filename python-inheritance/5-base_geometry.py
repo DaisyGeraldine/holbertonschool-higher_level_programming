28e6a66b90001bcd40ed736b982f0cb2fa3a805e
DaisyGeraldine <chipanal@hotmail.com>
2022-09-29 17:44:17 -0500

    Write an empty class BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module named 5-base_geometry.py
+   Created on Thursday, September 29, 2022
+   @author: Daisy Chipana Lapa
+"""
+
+
+class BaseGeometry:
+    """This class BaseGeometry
+    Attributes:
+       Nothing
+    """
+    pass
