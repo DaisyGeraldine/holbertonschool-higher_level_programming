6b412b2ba069c4528f671f422ce11229e9cf831e
DaisyGeraldine <chipanal@hotmail.com>
2022-09-29 12:11:38 -0500

    function that returns the list of available attributes and methods of an object

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This module named 0-rectangle.py
+   Created on Wednesday, September 28, 2022
+   @author: Daisy Chipana Lapa
+"""
+
+
+def lookup(obj):
+    """Function named lookup
+          Args:
+               obj: Parameter object
+
+          Returns:
+               Nothing
+    """
+    print(dir(obj))
