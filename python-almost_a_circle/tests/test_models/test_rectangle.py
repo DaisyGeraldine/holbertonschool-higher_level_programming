dd062a34ba4bc76c14b33b25a7828fefb1413561
DaisyGeraldine <chipanal@hotmail.com>
2022-10-05 21:22:56 -0500

    try test

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,16 +6,16 @@ from models.rectangle import Rectangle
 
 
 class Test_Rectangle(unittest.TestCase):
-    def test_Rectangle_two_arguments(self):
-        """This methods will be tested with two_arguments"""
-        r1 = Rectangle(10, 2)
-        r2 = Rectangle(2, 10)
-        self.assertEqual(r1.id, 1)
-        self.assertEqual(r2.id, 2)
-
     def test_Rectangle_all_arguments(self):
         """This methods will be tested with all_arguments"""
         r1 = Rectangle(10, 2, 0, 0, 12)
         r2 = Rectangle(2, 10, 0, 0, 5)
         self.assertEqual(r1.id, 12)
         self.assertEqual(r2.id, 5)
+
+    def test_Rectangle_two_arguments(self):
+        """This methods will be tested with two_arguments"""
+        r1 = Rectangle(13, 2)
+        r2 = Rectangle(5, 4)
+        self.assertEqual(r1.id, 1)
+        self.assertEqual(r2.id, 2)
