0d5f300031deb7e146b9c30cf73a1f7707c175da
DaisyGeraldine <chipanal@hotmail.com>
2022-10-08 11:02:13 -0500

    test pass

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -17,5 +17,32 @@ class Test_Rectangle(unittest.TestCase):
         """This methods will be tested with two_arguments"""
         r1 = Rectangle(13, 2)
         r2 = Rectangle(5, 4)
+        r1.id = 1
         self.assertEqual(r1.id, 1)
+        r2.id = 2
         self.assertEqual(r2.id, 2)
+
+class Test_Rectangle_validate_atributes(unittest.TestCase):
+    def test_Rectangle_str(self):
+        """This methods will be tested with str value"""
+        with self.assertRaises(TypeError):
+            r1 = Rectangle("1", 2)
+        with self.assertRaises(TypeError):
+            r2 = Rectangle(1, "2")
+
+    def test_Rectangle_negative_or_zero(self):
+        """This methods will be tested with a negative number"""
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(10, 2)
+            r1.width = -10
+        with self.assertRaises(ValueError):
+            r1 = Rectangle(10, 2)
+            r1.width = 0
+
+class Test_Rectangle_Area_first(unittest.TestCase):
+    def test_Rectangle_area_number_positive(self):
+        """This methods will be tested with numbers positive"""
+        r1 = Rectangle(3, 2)
+        self.assertEqual(r1.area(), 6)
+        r2 = Rectangle(8, 7, 0, 0, 12)
+        self.assertEqual(r2.area(), 56)
