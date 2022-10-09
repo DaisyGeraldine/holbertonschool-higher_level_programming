34a91062205233ea7041eec77dc879eb8beca975
DaisyGeraldine <chipanal@hotmail.com>
2022-10-09 16:09:21 -0500

    push test rentangle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -8,8 +8,8 @@ from models.rectangle import Rectangle
 class Test_Rectangle(unittest.TestCase):
     def test_Rectangle_arguments_exist(self):
         """This methods will be tested with all_arguments"""
-        r1 = Rectangle(10, 2, 1, 1, 12)
-        #self.assertEqual(r1, (10, 2, 0, 0, 12))
+        r1 = Rectangle(1, 2)
+        self.assertEqual((r1.width, r1.height), (1, 2))
 
     def test_Rectangle_all_arguments(self):
         """This methods will be tested with all_arguments"""
