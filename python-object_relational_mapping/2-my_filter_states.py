4ded4a0218cb588e664f3bf72a1abcac10f79f4e
DaisyGeraldine <chipanal@hotmail.com>
2022-11-12 14:51:52 -0500

    fix: takes in an argument and displays all values in the states - Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8mb4")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE BINARY "
+                "name = BINARY '{}' ORDER BY id ASC".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
