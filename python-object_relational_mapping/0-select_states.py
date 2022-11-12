44d88381f0e408e5ce8b7f8497a789c0b531b4c5
DaisyGeraldine <chipanal@hotmail.com>
2022-11-12 01:21:30 -0500

    fix : script that lists all states from the database hbtn_0e_0_usa

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """This module lists all states from the database hbtn_0e_0_usa
    Created on Saturday, November 12, 2022
-   @author: Daisy Chipana Lapa
+   @author: DaisyG Chipana Lapa
 """
-
 
 import MySQLdb
 from sys import argv
-conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+
+conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                       passwd=argv[2], db=argv[3], charset="utf8")
 cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
+
+cur.execute("SELECT * FROM states ORDER BY id ASC")
 query_rows = cur.fetchall()
 for row in query_rows:
     print(row)
