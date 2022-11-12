fb35dc7948cd03bfc8027ebd9b52ae41128dc6cd
DaisyGeraldine <chipanal@hotmail.com>
2022-11-12 14:42:55 -0500

    takes in an argument and displays all values in the states - Filter states by user input

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This module takes in an argument and displays all values in the states table
+   of hbtn_0e_0_usa where name.
+   Created on Saturday, November 12, 2022
+   @author: DaisyG Chipana Lapa
+"""
+
+if __name__ == '__main__':
+    import MySQLdb
+    from sys import argv
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8mb4")
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM states WHERE name = '{argv[4]}'")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
