c5530d733a465453abd6edd1aeb347d556945ff4
DaisyGeraldine <chipanal@hotmail.com>
2022-11-12 23:02:27 -0500

    fix - write one that is safe from MySQL injections!

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module takes in an argument and displays all values in the states table
-   of hbtn_0e_0_usa where name.Code write one that is safe from MySQL injections
+   of hbtn_0e_0_usa where name.
+   Code write one that is safe from MySQL injections.
    Created on Saturday, November 12, 2022
    @author: DaisyG Chipana Lapa
 """
