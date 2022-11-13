972fa5a6167a8ab51f746426be2439e49fad243a
DaisyGeraldine <chipanal@hotmail.com>
2022-11-13 18:59:52 -0500

    prints the State object with the name passed as argument - Get a state

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument
+   from the database hbtn_0e_6_usa.
+   Created on Sunday, December 13, 2022
+   @author: DaisyG Chipana Lapa
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import (sessionmaker)
+
+if __name__ == "__main__":
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), echo=False)
+    Base.metadata.create_all(engine)
+
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # create a Session
+    session = Session()
+
+    cont = session.query(State).filter_by(name=sys.argv[4]).count()
+
+    if cont == 0:
+        print("Not found")
+    else:
+        for state in session.query(State).filter_by(name=sys.argv[4]):
+            print("{}".format(state.id))
+
+    session.close()
