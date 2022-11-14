ecbc0b046854973fac9bf52930d667aa11ddcc9c
DaisyGeraldine <chipanal@hotmail.com>
2022-11-13 22:13:31 -0500

    add foreign key file model_city

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -6,7 +6,7 @@
 """
 
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 
@@ -16,4 +16,4 @@ class City(Base):
     __tablename__ = 'cities'
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer)
+    state_id = Column(Integer, ForeignKey('states.id'))
