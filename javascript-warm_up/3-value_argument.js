3de9dc4fac03c192d0bdb1765e4a1ced9ba1a879
DaisyGeraldine <chipanal@hotmail.com>
2022-12-11 11:55:21 -0500

    script that prints the first argument passed to it

---
+++ javascript-warm_up/3-value_argument.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// import { argv } from 'node:process';
+const process = require('process');
+
+const args = process.argv;
+let count = 0;
+// print process.argv
+args.forEach((val, index) => {
+  count = index + 1;
+});
+if (count === 2) {
+  console.log('No argument');
+} else if (count > 2) {
+  console.log(process.argv[2]);
+}
