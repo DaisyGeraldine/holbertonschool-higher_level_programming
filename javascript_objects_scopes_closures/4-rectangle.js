8d65b201291de92df7d33b86a6b3307bc0950f72
DaisyGeraldine <chipanal@hotmail.com>
2022-12-11 22:19:28 -0500

    Create an instance method called rotate() and instance method called double() that multiples the width and the height of the rectangle by 2

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (parseInt(w) > 0 && parseInt(h) > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const newWidth = this.width;
+    const newHeight = this.height;
+    this.width = newHeight;
+    this.height = newWidth;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
