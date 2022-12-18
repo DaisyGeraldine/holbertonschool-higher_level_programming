e6d30a9767ae7f868c51587510487c53a7191e62
DaisyGeraldine <chipanal@hotmail.com>
2022-12-18 12:58:06 -0500

    The <header> element must always have one class: red or green, never both in the same time and never empty

---
+++ javascript-web_jquery/4-script.js
@@ -0,0 +1,9 @@
+$(function ($) {
+  $('DIV#toggle_header').on('click', function (event) {
+    if ($('header').hasClass('green')) {
+      $('header').removeClass('green').addClass('red');
+    } else {
+      $('header').removeClass('red').addClass('green');
+    }
+  });
+});
