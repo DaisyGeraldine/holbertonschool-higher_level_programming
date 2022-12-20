0d1b82e1816ce9147fdfd002d70407848df3d7e6
DaisyGeraldine <chipanal@hotmail.com>
2022-12-20 16:28:56 -0500

    fix: change removeclass and addclass by toggleclass

--- javascript-web_jquery/4-script.js
+++ javascript-web_jquery/4-script.js
@@ -1,9 +1,14 @@
 $(function ($) {
   $('DIV#toggle_header').on('click', function (event) {
+      $('header').toggleClass('green').addClass('red');
+  });
+});
+/*$(function ($) {
+  $('DIV#toggle_header').on('click', function (event) {
     if ($('header').hasClass('green')) {
       $('header').removeClass('green').addClass('red');
     } else {
       $('header').removeClass('red').addClass('green');
     }
   });
-});
+});*/
