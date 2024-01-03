08110eaa250e982d3543aadcfbc65db9f37042f2
Tachi <105659277+DaisyGeraldine@users.noreply.github.com>
2024-01-03 17:40:13 -0500

    Delete python-more_data_structures directory

--- python-more_data_structures/6-print_sorted_dictionary.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    for i in range(len(a_dictionary)):
-        print('{}: {}'.format(sorted(a_dictionary)[i],
-                              a_dictionary[sorted(a_dictionary)[i]]))
