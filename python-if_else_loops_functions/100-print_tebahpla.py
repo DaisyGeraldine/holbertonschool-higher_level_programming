ba6f705e5917d5ff1e3ddab196fa1f2d72eedca9
Tachi <105659277+DaisyGeraldine@users.noreply.github.com>
2024-01-03 17:39:08 -0500

    Delete python-if_else_loops_functions directory

--- python-if_else_loops_functions/100-print_tebahpla.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-for i in range(-122, -96):
-    char = i * -1
-    if i % 2 == 0:
-        print('{}'.format(chr(char)), end="")
-    else:
-        print('{}'.format(chr(char - 32)), end="")
--- python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for char in str:
-        if (ord(char) >= 97 and ord(char) <= 122):
-            char_upper = ord(char) - 32
-        else:
-            char_upper = ord(char)
-        print('{}'.format(chr(char_upper)), end="")
-    print('{}'.format(""))
