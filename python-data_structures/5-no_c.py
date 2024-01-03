a5e23e4e10ef3210bd4379c89fbb53944818305a
Tachi <105659277+DaisyGeraldine@users.noreply.github.com>
2024-01-03 17:39:54 -0500

    Delete python-data_structures directory

--- python-data_structures/5-no_c.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    str = ""
-    for i in my_string:
-        if i != chr(67) and i != chr(99):
-            str = str + i
-    return str
--- python-data_structures/8-multiple_returns.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def multiple_returns(sentence):
-    if sentence == "":
-        first_c = None
-    else:
-        first_c = sentence[0]
-    len_str = len(sentence)
-    return(len_str, first_c)
