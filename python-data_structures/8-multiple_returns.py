243b71301fb81e68dda7d71eab27418dec7e7f64
DaisyGeraldine <chipanal@hotmail.com>
2024-01-08 00:18:59 -0500

    Modify task8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        first_c = None
+        one_char = None
     else:
-        first_c = sentence[0]
+        one_char = sentence[0]
     len_str = len(sentence)
-    return(len_str, first_c)
+    return(len_str, one_char)
