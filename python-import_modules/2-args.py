473124b6aa5d918b9a16d36f324ff0d4c18adb28
DaisyGeraldine <chipanal@hotmail.com>
2024-01-07 20:45:01 -0500

    Modify task2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,6 +3,6 @@ from sys import argv
 len_arg = len(argv) - 1
 print("{} argument{}"
       .format(len_arg, "s." if len_arg == 0
-              else "" if len_arg == 1 else "s:"))
+              else ":" if len_arg == 1 else "s:"))
 for i in range(len_arg):
     print("{}: {}".format(i + 1, argv[i + 1]))
