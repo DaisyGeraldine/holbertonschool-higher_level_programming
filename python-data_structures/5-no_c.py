296651f8b1f543d41de999b80849128a85aa314f
DaisyGeraldine <chipanal@hotmail.com>
2024-01-07 23:33:01 -0500

    Can you C me now?, task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new_str = ""
     for i in my_string:
         if i == "c" or i == "C":
             continue
-        print("{}".format(i), end="")
+        new_str += i
+    return new_str
