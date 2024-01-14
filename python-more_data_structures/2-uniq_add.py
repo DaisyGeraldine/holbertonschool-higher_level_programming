0d49fb6083bb2364dd17933603b298bd77311e0d
DaisyGeraldine <chipanal@hotmail.com>
2024-01-14 16:29:29 +0000

    fix: modify task

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -4,7 +4,6 @@ def uniq_add(my_list=[]):
     max_item = len(my_list)
     new_list = sorted(my_list)
     for item in range(max_item):
-        print(new_list[item])
         if item + 1 < max_item:
             if new_list[item] != new_list[item + 1]:
                 sum += new_list[item]
