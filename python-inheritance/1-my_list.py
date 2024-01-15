d038c4592fa08899618a5455e46c5d0cf744f5bb
DaisyGeraldine <chipanal@hotmail.com>
2024-01-15 00:45:07 +0000

    class MyList that inherits from list, task1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Task 1 : My list """
+
+
+class MyList(list):
+    """class MyList that inherits from list """
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort) """
+        return print(sorted(self))
