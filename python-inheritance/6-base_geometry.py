094ba207c05397a9938f08e48cdadb344a268834
DaisyGeraldine <chipanal@hotmail.com>
2024-01-15 03:04:43 +0000

    nstance method: def area(self): that raises an Exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Task 6 : Improve Geometry """
+
+
+class BaseGeometry:
+    """BaseGeometry class """
+    def area(self):
+        """that raises an Exception with the message """
+        raise Exception('area() is not implemented')
