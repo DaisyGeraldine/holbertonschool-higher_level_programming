d81638a309e118ecb1dd943b32c86c9739f89632
DaisyGeraldine <chipanal@hotmail.com>
2024-01-15 04:53:05 +0000

    feat: modify document

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Task 11 : Square #2 """
+"""Task 0 : Read file """
 
 
 def read_file(filename=""):
     """function that reads a text file (UTF8) and prints it to stdout """
-    with open(filename) as f:
+    with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
