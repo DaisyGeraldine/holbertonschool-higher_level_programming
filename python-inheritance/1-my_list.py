33feaa3898fcfddcef9a2e33f15905f575d2d689
DaisyGeraldine <chipanal@hotmail.com>
2024-01-15 00:52:52 +0000

    modify doc

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-""" Task 1 : My list """
+"""Task 1 : My list """
 
 
 class MyList(list):
-    """class MyList that inherits from list """
+    """class MyList that inherits from list
+        Attributes:
+            Public instance method: print_sorted
+    """
     def print_sorted(self):
         """prints the list, but sorted (ascending sort) """
         return print(sorted(self))
