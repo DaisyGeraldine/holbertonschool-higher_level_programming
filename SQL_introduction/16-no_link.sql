2d962715425e33232e45eadc052f03d5fc140739
DaisyGeraldine <chipanal@hotmail.com>
2024-01-28 16:59:52 -0500

    Records should be listed by descending score

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
 -- script that lists all records of the table second_table, don’t list rows without a name value
 SELECT score, name
     FROM second_table
-    WHERE name IS NOT NULL;
+    WHERE name IS NOT NULL
+    ORDER BY score DESC;
