140242a1f21d4e071e830f13fee1340d0490c3aa
DaisyGeraldine <chipanal@hotmail.com>
2024-01-28 23:05:47 -0500

    function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Task 8 : Class to JSON """
+
+
+
+def class_to_json(obj):
+    """function named load_from_json_file
+           Args:
+               obj: Is an instance of a Class
+           Returns:
+               The dictionary description with simple data structure
+    """
+    return {key: value for (key, value) in obj.__dict__.items()
+            if key in list(obj.__dict__.keys())}
